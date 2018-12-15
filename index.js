b589ae40f1d305d02321395119747948b5358787
Anayo Oleru <anayo_oleru@outlook.com>
2018-12-15 14:02:23 -0800

    [feature]add and test the integral parts

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname + '/styles'));
-app.use("/images", express.static(_dirname + '/images'));
-app.use("/scripts", express.static(_dirname + '/scripts'));
+app.use("/images", express.static(__dirname + '/images'));
+app.use("/scripts", express.static(__dirname + '/scripts'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'views/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + 'views/about.html'));
+        res.sendFile(path.join(__dirname + '/views/about.html'));
 })
 
-app.listen(process.env.PORT || 8080);
+// Handle 404: send an 404 error page
+app.use(function(req, res) {
+    res.status(404).sendFile(path.join(__dirname + '/errors/404.html'));
+ });
+ 
+ // Handle 500: send a 500 error
+ app.use(function(error, req, res, next) {
+    res.status(500).sendFile(path.join(__dirname + '/errors/500.html'));
+ });
+
+
+app.listen(process.env.PORT || 8080,  function(){
+    console.log("Oh yes, go on with your request, I am listening!")
+});
